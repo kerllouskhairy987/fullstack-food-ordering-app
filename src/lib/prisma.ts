0b693ab801ac4fa490f84e8b8666d1897b4f8d21
kerllouskhairy987 <kyrlskhyry80@gmail.com && git config --global user.name kerllouskhairy987git config --global user.email kyrlskhyry80@gmail.com>
2025-09-20 18:43:27 +0300

    fix

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,5 +1,6 @@
 import { Environments } from '@/constants/enums';
-import { PrismaClient } from '../../generated/prisma';
+// import { PrismaClient } from '../../generated/prisma';
+import { PrismaClient } from '@prisma/client'
 
 // PrismaClient is attached to the `global` object in development to prevent
 // exhausting your database connection limit.
